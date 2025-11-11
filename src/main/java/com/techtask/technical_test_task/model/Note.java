407ad18eefa8b6573ae59ae6022d48efa018bdc0
chebelok <lotokhinivan@gmail.com>
2025-11-12 01:20:23 +0200

    added more validation to the Note model

--- src/main/java/com/techtask/technical_test_task/model/Note.java
+++ src/main/java/com/techtask/technical_test_task/model/Note.java
@@ -1,6 +1,7 @@
 package com.techtask.technical_test_task.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,12 +23,15 @@ public class Note {
     private String id;
 
     @NotBlank(message = "Title cannot be empty")
+    @Size(max = 200, message = "Title cannot exceed 200 characters")
     private String title;
 
     private LocalDateTime createDate;
 
     @NotBlank(message = "Text cannot be empty")
+    @Size(max = 5000, message = "Text cannot exceed 5000 characters")
     private String text;
 
+    @Size(max = 3, message = "Cannot have more than 3 tags")
     private List<Tag> tags;
 }
